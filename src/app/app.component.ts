4b2d122aaab288dc5e889e6d9fded23f8663b460
Erica <contacteljones@gmail.com>
2020-04-13 19:08:50 -0500

    comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+
+
+sourceList : Satellite[] = [];
+displayList : Satellite [] = [];
+
+constructor() {
+  this.sourceList = [];
+  let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+  window.fetch(satellitesUrl).then(function(response) {
+     response.json().then(function(data) {
+      
+      let fetchedSatellites = data.satellites;
+        for (let i in fetchedSatellites){
+        let temp = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+        this.sourceList.push(temp);
+        
+        }
+        this.displayList = this.sourceList.slice(0);
+        // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+    
+     }.bind(this));
+  }.bind(this))
+
+}
 }
